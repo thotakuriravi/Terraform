15bca039efc31697ac3e9b744a9b69e37fb0d668
thotakuriravi <thotakuriravi@gmail.com>
2022-10-24 01:06:46 +0530

    after Outputs

---
+++ 02_variables/command_type/readme.md
@@ -0,0 +1,4 @@
+
+terraform plan -var=instance_type="t2-nano"
+                                  "t2-micro"
+                                  "m1-small"
--- 03_locals/main.tf
+++ 03_locals/main.tf
@@ -1,11 +1,16 @@
+
 variable "instance_type" {
   type        = string
   description = "In this file i am going to create the tfvar file and saving the variable names in that file"
-
+  
   validation {
     condition = length(var.instance_type) > 4 && substr(var.instance_type, 0, 4)  == "ami-"
     error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
   }
+}
+
+locals {
+  project_name = "ravikumar"
 }
 
 
@@ -14,6 +19,9 @@ resource "aws_instance" "terraform123" {
   ami           = "ami-40d28157"
   instance_type = var.instance_type
   tags = {
-    Name = "Terraform123"
+    Name = "Terraform123-${local.project_name}"
   }
 }
+
+#       ${local.project_name}  --> we write for the local variables and
+#       #{local/}  --> we write for the path
